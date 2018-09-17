514fbf28ddd3eabd8e7bea469712cda54326307c
Barry Britt [LAS] <bbritt@iastate.edu>
2018-09-17 14:20:32 -0500

    update printf to include flush
    Signed-off-by: Barry Britt [LAS] <bbritt@iastate.edu>

--- lib/printf.py
+++ lib/printf.py
@@ -10,24 +10,24 @@ ENDC = '\033[0m'
 def OK(count, msg):
 	if count >= 0:
 		print GREEN + "[OK] " + msg + ENDC
-		sys.stdout.flush()
+	sys.stdout.flush()
 
 def ERROR(count, msg):
-    if count >= 0:
-	    print RED + "[ERROR] " + msg + ENDC
-		sys.stdout.flush()
+	if count >= 0:
+		print RED + "[ERROR] " + msg + ENDC
+	sys.stdout.flush()
 
 def WARNING(count, msg):
-    if count >= 1:
-	    print WARNING + "[WARNING] " + msg + ENDC
-		sys.stdout.flush()
+	if count >= 1:
+		print WARNING + "[WARNING] " + msg + ENDC
+	sys.stdout.flush()
 
 def INFO(count, msg):
 	if count >= 1:
 		print BLUE + "[INFO] " + msg + ENDC
-		sys.stdout.flush()
+	sys.stdout.flush()
 
 def DEBUG(count, msg):
 	if count >= 3:
 		print PURPLE + "[DEBUG]" + msg + ENDC
-		sys.stdout.flush()
+	sys.stdout.flush()
