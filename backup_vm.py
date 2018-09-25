22942ba94078e7d99ce6e00c4f21de69c2a4ca76
Barry Britt <barry.britt@gmail.com>
2018-09-25 13:57:30 -0500

    Updating backup

--- backup_vm.py
+++ backup_vm.py
@@ -277,8 +277,9 @@ def backup(vmid, snapid, disk_id, bkpid):
     try:
         printf.DEBUG(args.debug, "Attach Output: " + str(attach_output))
     except:
+        print "Could not print output"
         # no op
-        
+
     printf.INFO(args.debug, "Identifying disk device (this might take a while)")
     dev = get_logical_disk(bkpid, disk_id)
     diskname = get_disk_name(vmid, snapid, disk_id)
